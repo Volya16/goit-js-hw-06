c5dbf8643a0dc71292c63bf9116a04b43a173846
Olya Vorontsova <16olya1996@gmail.com>
2023-09-02 15:31:02 +0200

    Update task 5
    task 5 is dane

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const inputField = document.querySelector("#name-input");
+const nameLabel = document.querySelector("#name-output");
+
+inputField.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+    const inputValue = event.currentTarget.value;
+
+    nameLabel.textContent = inputValue === "" ? 'Anonymous' : inputValue;
+}
