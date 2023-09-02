5b282bd7096a82d68b364ef5c61dd74f8449ee53
Olya Vorontsova <16olya1996@gmail.com>
2023-09-02 15:35:59 +0200

    update task 10
    task 10 is done

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,40 @@
+const input = document.querySelector('input[type="number" ]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesDiv = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+
+  for (let i = 0; i <amount; i ++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    const color = getRandomHexColor();
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = color;
+
+    boxesDiv.append(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+  input.value = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (!isNaN(amount)) {
+    createBoxes(amount);
+  }
+});
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
