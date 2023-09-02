0358482ad9e222f1b1dfbefcc2c6b031321eecff
Olya Vorontsova <16olya1996@gmail.com>
2023-09-02 15:20:47 +0200

    update 2 task
    task 2 ist done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+console.log(list);
+
+const markup = ingredients.map(element => {
+  const li = document.createElement('li')
+  li.textContent = element;
+  li.classList.add('item')
+  console.log(li);
+  console.log(element);
+  return li
+})
+
+list.append(...markup);
+console.log(markup);
