654ab0381e24a2e11d42107f5e1e25c53f711089
Olya Vorontsova <16olya1996@gmail.com>
2023-09-02 15:35:18 +0200

    update task 9
    task 9 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const spanColor = document.querySelector(".color");
+console.log(spanColor);
+const buttonChangeColor = document.querySelector(".change-color");
+console.log(buttonChangeColor);
+
+buttonChangeColor.addEventListener("click", onClickButton);
+
+function onClickButton(event) {
+  document.body.style.backgroundColor = spanColor.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
