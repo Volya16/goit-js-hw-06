8880ed50bd363380586f51fdbfb105ed166ef426
Olya Vorontsova <16olya1996@gmail.com>
2024-12-04 22:59:33 +0100

    added third task

--- js/task-3.js
+++ js/task-3.js
@@ -1,30 +1,34 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return balance;
-  },
-  getDiscount() {
-    return discount;
-  },
-  setDiscount(value) {
-    discount = value;
-  },
-  getOrders() {
-    return orders;
-  },
-  addOrder(cost, order) {
-    balance -= cost - cost * discount;
-    orders.push(order);
-  },
-  // Change code above this line
-};
+class StringBuilder {
+    #value;
+    
+    constructor(params) {
+        this.#value = params;
+        
+    }
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+    getValue() {
+        return this.#value
+    }
+
+    padEnd(str) {
+        this.#value += str;
+    }
+
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
